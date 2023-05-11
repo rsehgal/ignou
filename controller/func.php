346b2e433faa3b6705031d92e31ca1bd31c5d9f3
Raman Sehgal <sc.ramansehgal@gmail.com>
2023-05-11 09:56:13 +0530

    Modified controller for signup function to store the data in the user_credentials table

--- controller/func.php
+++ controller/func.php
@@ -1,11 +1,23 @@
 <?php
+require "../model/Symposia.php";
 function my_function() {
   // Your function logic here
   $data = array("Hello", "Ha ha aha d");
   return json_encode(array("data" => implode(" ", $data)));
 }
 function ServeSignup(){
-	       
+		$obj = new DB();
+		$obj->Set('127.0.0.1','sympadmin','sympadmin','symposia');
+		$obj->Connect();	
+ 		$firstname = $_POST['firstname'];
+		$lastname = $_POST['lastname'];
+		$email=$_POST['email'];
+		$username=$_POST['username'];
+		$password=$_POST['password'];
+		$query = "insert into user_credentials values('$username','$password','$firstname','$lastname','$email')";
+		echo $query."<br/>";
+		$obj->GetQueryResult($query);
+		echo "$username : $password : $firstname : $lastname : $email";	
                return "<div>ServeSignup function called..........</div><br/>".$_POST['firstname'];
 }
 
