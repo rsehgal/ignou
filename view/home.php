80964eb3899344d9ee7b1054dbb925c5c9a65226
Raman Sehgal <sc.ramansehgal@gmail.com>
2023-05-26 13:11:13 +0530

    Corrected the title that display on web browser.

--- view/home.php
+++ view/home.php
@@ -5,7 +5,9 @@ session_start();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Bootstrap Menu Example</title>
+<title>
+The National Academy of Sciences, India, NASI-2023.
+</title>
   <!-- Bootstrap CSS -->
 <!--  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" > -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
