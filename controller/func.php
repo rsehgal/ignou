c2ba95c334f2e8d7d72c4c0aeee291cf3582fecc
Raman Sehgal <sc.ramansehgal@gmail.com>
2023-06-05 10:22:19 +0530

    Added a warning bg color to those text box where coordinator are not yet alloted.

--- controller/func.php
+++ controller/func.php
@@ -1288,7 +1288,10 @@ function Allot(){
 		$retTable.='<td>'.$selectedCategory.'</td>';
 		$retTable.='<td><a href="../'.$_SESSION["uploadlocation"].'/'.$fileName.'">'.$fileName.'</a></td>';
 		//$retTable.='<td><textarea class="form-control" id="remarks_'.$updateButtonId.'">'.$remarks.'</textarea></td>';
-		$retTable.='<td>'.AddDecisionEntries($decArray,"Decision",$updateButtonId).'
+		$retTable.='<td>'.AddDecisionEntries($decArray,"Decision",$updateButtonId);
+		if($status=="")
+				<input type="text" id="decisionText_'.$updateButtonId.'" value="'.$status.'" class="form-control bg-warning"/></td>';
+		else
 				<input type="text" id="decisionText_'.$updateButtonId.'" value="'.$status.'" class="form-control"/></td>';
 		$retTable.='<td><input type="button" id="'.$updateButtonId.'" class="btn btn-primary updateDecision" value="Update" functionName="'.$allotmentType.'"/></td>';
 		$retTable.='</tr>';
