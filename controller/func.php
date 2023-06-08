d9e688241313b93985da2fb2054823c63ba2381e
Raman Sehgal <sc.ramansehgal@gmail.com>
2023-06-08 16:05:27 +0530

    Still tying.....

--- controller/func.php
+++ controller/func.php
@@ -325,7 +325,7 @@ function ServeLogin(){
 		$loginStatusMsg='<h4><mark >Logged in as : '.$_SESSION["username"].'</mark> <input type="button" class="btn btn-custom btn-danger" id="logout" value="Logout"/></h4>';
 		$localJs = '<script>
 				$(function(){
-				$("#loginstatus").html("<h4><mark>Logged in as : '.$_SESSION["username"].'</mark></h4>")});
+				$("#loginstatus").html("<h4><mark>Logged in as : '.$_SESSION["username"].'</mark><input type="button" class="btn btn-custom btn-danger" id="logout" value="Logout"/> </h4>")});
 				</script>';	
 				//$("#loginstatus").html('.$loginStatusMsg.')});
 		return $localJs." <div><h3 class='alert alert-success' role='alert'> Welcome ".$_SESSION["logintype"]." : ".$uname."</h3><br/>".$loginStatusMsg.'<br/>'.Referee_UpdatePaperStatus();
