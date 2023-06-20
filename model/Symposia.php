1e0b250397e69abd8da5fb1a2eee5ddc8b8acd20
Raman Sehgal <sc.ramansehgal@gmail.com>
2023-06-20 09:52:57 +0530

    Added a class to have the sticky navigation bar.

--- model/Symposia.php
+++ model/Symposia.php
@@ -171,7 +171,7 @@ function Menu(){
 </div>';
 
 
-$result='<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+$result='<nav class="navbar navbar-expand-lg navbar-light bg-dark sticky-top">
        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>';
